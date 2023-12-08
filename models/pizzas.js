1bc9d838210ad4c1db6c5812890ab5700cd6cc1d
Alberto <malasi.alberto@gmail.com>
2023-12-08 12:14:26 +0100

    test heroku miglioramento codice per via di crash

--- models/pizzas.js
+++ models/pizzas.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // const uri = "mongodb+srv://albyzeta:1234@nodetuts.bfsfj3y.mongodb.net/Menu";
-
+const pass = process.env.MONGODB_PASS;
+const uri = "mongodb+srv://albyzeta:"+pass+"@nodetuts.bfsfj3y.mongodb.net/Menu"
 mongoose.connect(uri)
 
 const pizza = new Schema ({
